447239990e09eac2419dc6b1b0138db23f153cea
Santiago Cecena <santiago.cecena@encora.com>
2025-02-10 18:23:37 -0700

    First Commit

---
+++ src/main/java/com/breakabletoy/app/BreakableToySantiagocecenaApplication.java
@@ -0,0 +1,13 @@
+package com.breakabletoy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakableToySantiagocecenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakableToySantiagocecenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakabletoy/app/BreakableToySantiagocecenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakabletoy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakableToySantiagocecenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
