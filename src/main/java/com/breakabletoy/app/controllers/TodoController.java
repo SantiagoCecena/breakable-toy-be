94ca7c0b3b2508e94e8f1a37559d9b6485d171fb
Santiago Cecena <santiago.cecena@encora.com>
2025-02-14 16:52:51 -0700

    feat: get request parameters in getTodos, implement update, mark and remove todo endpoints

--- src/main/java/com/breakabletoy/app/controllers/TodoController.java
+++ src/main/java/com/breakabletoy/app/controllers/TodoController.java
@@ -3,7 +3,10 @@ package com.breakabletoy.app.controllers;
 
 import com.breakabletoy.app.models.Todo;
 import com.breakabletoy.app.services.TodoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/todos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TodoController {
 
     @Autowired
@@ -19,32 +23,59 @@ public class TodoController {
 
     // Get all todos
     @GetMapping
-    public List<Todo> getAllTodos() {
-        return todoService.findAll();
-    }
-
-    // Get a todo by id
-    @GetMapping("/{id}")
-    public Optional<Todo> getTodoById(@PathVariable UUID id) {
-        return todoService.findById(id);
+    public ResponseEntity<List<Todo>> getAllTodos(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "") String name,
+            @RequestParam(defaultValue = "all") String priority,
+            @RequestParam(defaultValue = "all") String done
+    ) {
+        return ResponseEntity.ok(todoService.findAll(page, name, priority, done));
     }
 
     // Create a new todo
     @PostMapping
-    public Todo createTodo(@RequestBody Todo todo) {
-        return todoService.save(todo);
+    public ResponseEntity<Todo> createTodo(@Valid @RequestBody Todo todo) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.save(todo));
+    }
+
+    // Update a todo
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable UUID id, @Valid @RequestBody Todo todo) {
+        Optional<Todo> updatedTodo = todoService.update(id, todo);
+        if (updatedTodo.isPresent()) {
+            return ResponseEntity.ok(updatedTodo.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Mark todo as done
+    @PostMapping("/{id}/done")
+    public ResponseEntity<Todo> markTodoAsDone(@PathVariable UUID id) {
+        Optional<Todo> markedTodo = todoService.markTodo(id, true);
+        if (markedTodo.isPresent()) {
+            return ResponseEntity.ok(markedTodo.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    // Mark todo as undone
+    @PutMapping("/{id}/undone")
+    public ResponseEntity<Todo> markTodoAsUndone(@PathVariable UUID id) {
+        Optional<Todo> markedTodo = todoService.markTodo(id, false);
+        if (markedTodo.isPresent()) {
+            return ResponseEntity.ok(markedTodo.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     // Remove a todo by id
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable UUID id) {
         todoService.deleteById(id);
-    }
-
-    // Update a todo
-    @PatchMapping("/{id}")
-    public Todo updateTodo(@RequestBody Todo todo) {
-        return todoService.update(todo);
     }
 
 }
